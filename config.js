72d57de2d2d1d93199b4c7ac6d6dcbbc7669f814
konrumi <konrumi@gmail.com>
2016-03-04 16:54:55 +0800

    prod config

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
 	home : 'http://10.213.1.167:10080/',
-	timeapi: 'http://admin.bi.ffan.com/security/interface/getDbTime',
+	timeapi: 'http://admin.bi.ffan.com/interface/getDbTime',
 	authapi: 'http://bi.ffan.com/'
 };
